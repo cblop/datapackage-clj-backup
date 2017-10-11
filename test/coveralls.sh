7595af7337787e64136f86d792c3350d922aecb3
cblop <commanderblop@gmail.com>
2017-10-11 17:06:27 +0100

    Changed coveralls script

--- test/coveralls.sh
+++ test/coveralls.sh
@@ -1,5 +1,2 @@
 COVERALLS_URL='https://coveralls.io/api/v1/jobs'
-CLOVERAGE_VERSION='1.0.9' lein cloverage -o cov --coveralls
-curl -F 'json_file=@cov/coveralls.json' "$COVERALLS_URL"
-echo "OUTPUT:"
-cat /home/travis/build/cblop/datapackage-clj/cov/coveralls.json
+CLOVERAGE_VERSION='1.0.9' lein cloverage -o cov --coveralls; curl -F 'json_file=@cov/coveralls.json' "$COVERALLS_URL"
