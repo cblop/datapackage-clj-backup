73d98e73da00c3f87f764550d9d0a76bd552fc47
cblop <commanderblop@gmail.com>
2017-10-11 16:54:23 +0100

    Modified coveralls.sh to debug

--- test/coveralls.sh
+++ test/coveralls.sh
@@ -1,3 +1,4 @@
 COVERALLS_URL='https://coveralls.io/api/v1/jobs'
 CLOVERAGE_VERSION='1.0.9' lein cloverage -o cov --coveralls
 curl -F 'json_file=@cov/coveralls.json' "$COVERALLS_URL"
+cat target/coverage/coveralls.json
