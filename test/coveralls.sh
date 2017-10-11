4c362655cc9e6236105f06448d8460d405b4bf8b
cblop <commanderblop@gmail.com>
2017-10-11 16:58:27 +0100

    More travis/coveralls debugging

--- test/coveralls.sh
+++ test/coveralls.sh
@@ -1,4 +1,5 @@
 COVERALLS_URL='https://coveralls.io/api/v1/jobs'
 CLOVERAGE_VERSION='1.0.9' lein cloverage -o cov --coveralls
 curl -F 'json_file=@cov/coveralls.json' "$COVERALLS_URL"
-cat target/coverage/coveralls.json
+echo "OUTPUT:"
+cat /home/travis/build/cblop/datapackage-clj/cov/coveralls.json
